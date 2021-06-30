f2a9885e954c888add64f1c8bce898a635e4b999
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-06-30 09:45:56 +0600

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,49 @@
+//math:
+
+
+var number;
+
+number = parseInt();
+number = prompt("Enter The number");
+
+if(number>=80){
+
+	console.log("You Got A+");
+}
+
+else if(number>=70){
+
+	console.log("You Got A");
+
+}
+
+
+else{
+
+	console.log("FAIL");
+}
+
+
+
+//EVEN and ODD Project
+
+var number;
+
+number = parseInt();
+number = prompt("Enter The number");
+
+if(number%2 == 0){
+
+	alert("EVEN");
+}
+
+
+
+else{
+
+	alert("ODD");
+}
+
+
+
+
